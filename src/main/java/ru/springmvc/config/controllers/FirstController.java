204d8925c4fb48d52b9ce159ebf8bddf9e817c03
Dmitrii Shitikov <vrnvgasu@yandex.ru>
2022-07-30 22:38:41 +0300

    6. Параметры GET запроса. Аннотация @RequestParam.

--- src/main/java/ru/springmvc/config/controllers/FirstController.java
+++ src/main/java/ru/springmvc/config/controllers/FirstController.java
@@ -3,12 +3,25 @@ package ru.springmvc.config.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first") // добавили перехват всех запросов на /first
 public class FirstController {
   @GetMapping("/hello")
-  public String helloPage() {
+  // HttpServletRequest - автоматом внедряет
+//  public String helloPage(HttpServletRequest request) {
+  public String helloPage(
+          @RequestParam(value = "name", required = false) String name,
+          @RequestParam(value = "surname", required = false) String surname
+          ) {
+//    String name = request.getParameter("name");
+//    String surname = request.getParameter("surname");
+
+    System.out.println("Hello, " + name + " " + surname);
+
     // хорошая практика - класть представления в каталог  контроллера
     return "first/hello";
   }
