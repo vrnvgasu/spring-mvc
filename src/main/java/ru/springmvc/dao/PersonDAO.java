d098f92564b026f26ab4c7854a41cf1a9a0ff7b1
Dmitrii Shitikov <vrnvgasu@yandex.ru>
2022-08-29 22:33:57 +0300

    13. SQL инъекции. PreparedStatement. JDBC API.

--- src/main/java/ru/springmvc/dao/PersonDAO.java
+++ src/main/java/ru/springmvc/dao/PersonDAO.java
@@ -2,6 +2,7 @@ package ru.springmvc.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -68,35 +69,67 @@ public class PersonDAO {
   }
 
   public Person show(int id) {
-//     return people.stream()
-//             .filter(person -> person.getId() == id)
-//             .findAny().orElse(null);
-    return null;
+    Person person = null;
+
+    try {
+      PreparedStatement preparedStatement =
+          connection.prepareStatement("SELECT * FROM person WHERE id=?");
+      preparedStatement.setInt(1, id);
+      ResultSet resultSet = preparedStatement.executeQuery();
+
+      resultSet.next(); // один раз сдвинем указатель, чтобы взять первую запись
+      person = new Person();
+      person.setId(resultSet.getInt("id"));
+      person.setName(resultSet.getString("name"));
+      person.setEmail(resultSet.getString("email"));
+      person.setAge(resultSet.getInt("age"));
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+
+    return person;
 
   }
 
   public void save(Person person) {
     try {
-      Statement statement = connection.createStatement();
-      // такой подход может привести к sql инъекциям
-      String sql = "INSERT INTO person VALUES( " +
-          1 + ",'" + person.getName() + "'," + person.getAge() +
-          ",'" + person.getEmail() + "')";
-      // executeUpdate ничего не возвращает
-      statement.executeUpdate(sql);
+      // подготавливает запрос и предотвращает sql инъекции
+      // на большим данных работает быстрее обычного Statement
+      PreparedStatement preparedStatement =
+          connection.prepareStatement("INSERT INTO person VALUES(1, ?, ?, ?");
+      preparedStatement.setString(1, person.getName());
+      preparedStatement.setInt(2, person.getAge());
+      preparedStatement.setString(2, person.getEmail());
+
+      // executeUpdate не возвращает результат
+      preparedStatement.executeUpdate();
     } catch (SQLException e) {
       throw new RuntimeException(e);
     }
   }
 
   public void update(int id, Person updatedPerson) {
-//    Person personToBeUpdated = show(id);
-//    personToBeUpdated.setName(updatedPerson.getName());
-//    personToBeUpdated.setAge(updatedPerson.getAge());
-//    personToBeUpdated.setEmail(updatedPerson.getEmail() );
+    try {
+      PreparedStatement preparedStatement =
+          connection.prepareStatement("UPDATE person set name=?, age=?, email=? WHERE id=?");
+      preparedStatement.setString(1, updatedPerson.getName());
+      preparedStatement.setInt(2, updatedPerson.getAge());
+      preparedStatement.setString(3, updatedPerson.getName());
+      preparedStatement.setInt(4, id);
+      preparedStatement.executeUpdate();
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
   }
 
   public void delete(int id) {
-//    people.removeIf(person -> person.getId() == id);
+    try {
+      PreparedStatement preparedStatement =
+          connection.prepareStatement("DELETE FROM person WHERE id=?");
+      preparedStatement.setInt(1, id);
+      preparedStatement.executeUpdate();
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
   }
 }
