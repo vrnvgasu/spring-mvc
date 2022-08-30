eb351c66802dc55b5357ad7f3ad201a4dfa29448
Dmitrii Shitikov <vrnvgasu@yandex.ru>
2022-08-30 23:33:20 +0300

    2. Пакетное обновление (Batch update)

---
+++ src/main/java/ru/springmvc/controllers/BatchController.java
@@ -0,0 +1,38 @@
+package ru.springmvc.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.springmvc.dao.PersonDAO;
+
+@Controller
+@RequestMapping("test-batch-update")
+public class BatchController {
+
+	private final PersonDAO personDAO;
+
+	@Autowired
+	public BatchController(PersonDAO personDAO) {
+		this.personDAO = personDAO;
+	}
+
+	@GetMapping()
+	public String index() {
+		return "batch/index";
+	}
+
+	@GetMapping("/without")
+	public String withoutBatch() {
+		// тестирует вставку отдельными записями
+		personDAO.testMultipleUpdate();
+		return "redirect:/people";
+	}
+
+	@GetMapping("/with")
+	public String withBatch() {
+		// тестирует вставку отдельными записями
+		personDAO.testBatchUpdate();
+		return "redirect:/people";
+	}
+}
--- src/main/java/ru/springmvc/dao/PersonDAO.java
+++ src/main/java/ru/springmvc/dao/PersonDAO.java
@@ -7,6 +7,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
@@ -63,4 +64,57 @@ public class PersonDAO {
   public void delete(int id) {
     jdbcTemplate.update("DELETE FROM person WHERE id=?", id);
   }
+
+  ///////////////////////////////
+  ///////// Тестируем производительность пакетной вставки
+  ///////////////////////////////
+
+  public void testBatchUpdate() {
+    List<Person> people = create1000People();
+
+    long before = System.currentTimeMillis();
+    // вставка через batch
+    jdbcTemplate.batchUpdate("INSERT INTO person VALUES(?, ?, ?, ?)", new BatchPreparedStatementSetter() {
+      @Override
+      // preparedStatement добавляет данные к запросу "INSERT INTO person VALUES(?, ?, ?, ?)"
+      // i счетчик от 0 до getBatchSize()-1
+      public void setValues(PreparedStatement preparedStatement, int i) throws SQLException {
+        preparedStatement.setInt(1, people.get(i).getId());
+        preparedStatement.setString(2, people.get(i).getName());
+        preparedStatement.setInt(3, people.get(i).getAge());
+        preparedStatement.setString(4, people.get(i).getEmail());
+      }
+
+      @Override
+      public int getBatchSize() {
+        return people.size();
+      }
+    });
+    long after = System.currentTimeMillis();
+
+    System.out.println("Time testBatchUpdate: " + (after - before));
+  }
+
+  public void testMultipleUpdate() {
+    List<Person> people = create1000People();
+
+    long before = System.currentTimeMillis();
+    people.forEach(person -> jdbcTemplate.update("INSERT INTO person VALUES(?, ?, ?, ?)",
+        person.getId(), person.getName(), person.getAge(), person.getEmail()));
+    long after = System.currentTimeMillis();
+
+    System.out.println("Time testMultipleUpdate: " + (after - before));
+  }
+
+  private List<Person> create1000People() {
+    List<Person> people = new ArrayList<>();
+
+    for (int i = 0; i < 1000; i++) {
+      Person person = new Person(i, "Name" + i, 20, "test" + i + "gmail.com");
+      people.add(person );
+    }
+
+    return people;
+  }
+
 }
